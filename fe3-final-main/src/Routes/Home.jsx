f0148924672d957591d1be96da8c14cb2285c3ac
VivianaGuzmanBuritica <82985780+VivianaGuzmanBuritica@users.noreply.github.com>
2023-04-05 23:44:49 -0500

    Update Home.jsx

--- fe3-final-main/src/Routes/Home.jsx
+++ fe3-final-main/src/Routes/Home.jsx
@@ -1,20 +1,12 @@
 import Card from '../Components/Card'
 import { useContextGlobal } from '../Components/utils/global.context'
-//import { useState, useEffect } from 'react'
+
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Home = () => {
 
-//  const [userList, setUserList] = useState([]);
-  // const fetchUser = async() => {
-  //   const res = await fetch('https://jsonplaceholder.typicode.com/users')
-  //   const data = await res.json()
-  //   setUserList(data)
-  // } 
-  // useEffect(() => {
-  //   fetchUser()
-  //   }, [])
+
 
  const {userList} = useContextGlobal()
  console.log("home "+userList);
